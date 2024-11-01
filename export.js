2efdc8071c7643a3c87b429217919c154f51604b
emmanuel <emmeillam2024@gmail.com>
2024-11-01 10:39:41 +0100

    assigment

--- export.js
+++ export.js
@@ -5,8 +5,32 @@ export let myInfo = {
     nationality : "Nigerian",
     stateOfOrigin : "Anambara",
     hobby : "music",
+    course: 'web-developer',
     phone : +8036081081,
     favFood : "rice"
 }
 
 console.log(myInfo)
+
+export let studentInfo = [{
+    name: "Daniel",
+    occupation: "product designer",
+    age: 22,
+    stateOfOrigin: "imo",
+    number: +90676756432,
+},
+{
+    name: "Jerry",
+    occupation: "data analytics",
+    age: 17,
+    stateOfOrigin: "taraba",
+    number: +90676756432,
+},
+{
+    name: "ogbu",
+    occupation: "product designer",
+    age: 20,
+    stateOfOrigin: "edo",
+    number: +90676756432,
+}]
+console.log(studentInfo);
