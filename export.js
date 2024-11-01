9ac14fc9d5f28f0947378d2c93184fc88ca17153
emmanuel <emmeillam2024@gmail.com>
2024-11-01 10:17:27 +0100

    assignment

---
+++ export.js
@@ -0,0 +1,12 @@
+export let myInfo = {
+    firstname : "Emmanuel",
+    lastname : "Azubuogu",
+    DOB : "16 Oct 2006",
+    nationality : "Nigerian",
+    stateOfOrigin : "Anambara",
+    hobby : "music",
+    phone : +8036081081,
+    favFood : "rice"
+}
+
+console.log(myInfo)
---
+++ import.js
@@ -0,0 +1,6 @@
+import {myInfo} from "./export.js"
+
+console.log(myInfo)
+
+let fullname = myInfo.firstname + " " + myInfo.lastname
+console.log(fullname);
